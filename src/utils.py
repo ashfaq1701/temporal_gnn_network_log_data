8fefb907c9ec549a8f0bd2499f603aae1f56e86f
Ashfaq Salehin <ashfaq.aws@gmail.com>
2024-07-02 21:57:36 +0100

    Retry functionality added

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import tarfile
 
 import requests
+from retrying import retry
 import os
 from urllib.parse import urlparse
 
@@ -16,15 +17,22 @@ def get_filename_from_url(url):
     return parsed_url.path.split('/')[-1]
 
 
+@retry(wait_exponential_multiplier=1000, wait_exponential_max=10000, stop_max_attempt_number=5)
 def download_file(url, dest_path):
-    filename = get_filename_from_url(url)
-    filepath = os.path.join(dest_path, filename)
+    try:
+        filename = get_filename_from_url(url)
+        filepath = os.path.join(dest_path, filename)
 
-    with requests.get(url, stream=True) as response:
-        response.raise_for_status()  # Check if the request was successful
-        with open(filepath, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=8192):  # Download in chunks
-                file.write(chunk)
+        with requests.get(url, stream=True) as response:
+            response.raise_for_status()  # Check if the request was successful
+            with open(filepath, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):  # Download in chunks
+                    file.write(chunk)
+
+        print(f"Downloaded file successfully: {filename}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading file from {url}: {e}")
+        raise
 
 
 def if_file_exists(dest_path, filename):

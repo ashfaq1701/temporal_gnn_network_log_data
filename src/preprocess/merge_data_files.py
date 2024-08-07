3aad7bd6b39ee220da5163806e295a075f7287d3
Ashfaq Salehin <ashfaq.aws@gmail.com>
2024-08-07 14:43:23 +0100

    Added logs

--- src/preprocess/merge_data_files.py
+++ src/preprocess/merge_data_files.py
@@ -13,14 +13,11 @@ def merge_data_files():
     num_nodes = len(filtered_node_features_encoder.classes_)
     num_node_features = int(os.getenv('N_NODE_FEATURES'))
     input_dir = os.getenv('FILTERED_DATA_DIR')
-
-    running_len_sum = 0
+    dataframes = []
 
     for idx in range(20160):
         filepath = os.path.join(input_dir, f'data_{idx}.parquet')
-        df = pd.read_parquet(filepath)
-        df.index = pd.RangeIndex(start=running_len_sum, stop=running_len_sum + len(df), step=1)
-        print(f'Read {filepath}')
+        dataframes.append(pd.read_parquet(filepath))
 
     merged_df = pd.concat(dataframes, ignore_index=True)
 

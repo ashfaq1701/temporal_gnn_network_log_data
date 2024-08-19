1cb81e30da1c8f6f6c4e1272b71b4298e236802c
Ashfaq Salehin <ashfaq.aws@gmail.com>
2024-08-19 11:30:26 +0100

    Fixed pickle file name

--- src/embedding/tgn/utils/utils.py
+++ src/embedding/tgn/utils/utils.py
@@ -119,18 +119,18 @@ def get_future_workloads(nodes, curr_minute, workloads, n_future):
 
 
 def get_past_workloads(nodes, curr_minute, workloads, n_past):
-    future_workloads = workloads[nodes, curr_minute - n_past + 1:curr_minute + 1]
-    padding_width = n_past - future_workloads.shape[1]
+    past_workloads = workloads[nodes, max(curr_minute - n_past + 1, 0):curr_minute + 1]
+    padding_width = n_past - past_workloads.shape[1]
 
     if padding_width > 0:
         padded_array = np.pad(
-            future_workloads,
+            past_workloads,
             pad_width=((0, 0), (0, padding_width)),
             mode='constant',
             constant_values=0.0
         )
     else:
-        padded_array = future_workloads
+        padded_array = past_workloads
     return padded_array
 
 

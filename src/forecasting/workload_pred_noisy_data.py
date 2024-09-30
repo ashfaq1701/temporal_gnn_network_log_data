6b2aec61da437c6114b01f85ff59be4d2c501646
Ashfaq Salehin <ashfaq.aws@gmail.com>
2024-09-30 01:06:10 +0100

    Initialize noisy data test

--- src/forecasting/workload_pred_noisy_data.py
+++ src/forecasting/workload_pred_noisy_data.py
@@ -1,2 +1,5 @@
-def test_workload_pred_noisy_data(model_path, test_microservice_id, reverse_test_data, num_runs, seeds):
+def test_workload_pred_noisy_data(
+        model_path, n_past, n_future, test_microservice_id,
+        reverse_test_data, num_runs, seeds
+):
     pass
